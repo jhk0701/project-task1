7f7f86a3648809bf9e488ef27bd3ea75f01e1379
jhk0701 <134210309+jhk0701@users.noreply.github.com>
2024-09-04 22:04:03 +0900

    feature-merch

--- Assets/Scripts/Npc.cs
+++ Assets/Scripts/Npc.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class Npc : MonoBehaviour
 {
-    
+    [SerializeField] Animator _anim;
+    int _idle;
+
+    void Start()
+    {
+        InvokeRepeating("SetRandomIdle", 3f, 3f);
+    }
+
+    void SetRandomIdle(){
+        _idle = Random.Range(0, 3);
+        _anim.SetInteger("Idle", _idle);
+        _anim.SetTrigger("Change");
+    }
 }
